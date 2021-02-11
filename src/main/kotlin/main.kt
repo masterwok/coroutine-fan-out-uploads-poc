5eb5216b17de6237d9df002c010e8eaeb33e2421
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2021-02-10 23:05:49 -0500

    PoC for cancelling coroutine processors

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,11 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
 
+private val mapping = mutableMapOf<Int, Job>()
+
 @ExperimentalCoroutinesApi
 private fun CoroutineScope.produceItems() = produce {
-    for(item in (0..4)) {
+    for(item in (0..1)) {
         println("Sending: $item")
         send(item)
     }
@@ -14,16 +16,42 @@ private fun CoroutineScope.produceItems() = produce {
     close()
 }
 
-fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
-    for (msg in channel) {
-        delay(4000)
-        println("Processor #$id processed $msg")
+private fun CoroutineScope.launchProcessor(processorId: Int, channel: ReceiveChannel<Int>) = launch {
+    for (item in channel) {
+        println("Processor #$processorId received $item")
+
+        val job = launch { uploadItem(item) }
+
+        mapping[item] = job
+
+        job.join()
+
+        if(!job.isCancelled) {
+            println("Processor #$processorId processed $item")
+        }
     }
 }
 
+private suspend fun uploadItem(item: Int) {
+    delay(4000)
+    println("Done uploading: $item")
+}
+
+private suspend fun cancelItem(item: Int) {
+    println(">>> cancelling $item <<<")
+
+    mapping[item]?.cancelAndJoin()
+    mapping.remove(item)
+}
+
 @ExperimentalCoroutinesApi
-suspend fun main(args: Array<String>) = withContext(Dispatchers.Default) {
+suspend fun main(args: Array<String>): Unit = withContext(Dispatchers.Default) {
     val receiver = produceItems()
 
-    repeat(2) { launchProcessor(it, receiver)}
+    repeat(1) { launchProcessor(it, receiver)}
+
+    launch {
+        delay(1000)
+        cancelItem(0)
+    }
 }
