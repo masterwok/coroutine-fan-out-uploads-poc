9e932486ec7e06263b9bf77303d694a89459fbce
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2021-02-10 22:38:53 -0500

    Initial fan-out implementation.

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,28 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+
+@ExperimentalCoroutinesApi
+private fun CoroutineScope.produceItems() = produce {
+    for(item in (0..4)) {
+        println("Sending: $item")
+        send(item)
+    }
+
+    println("Closing producer...")
+
+    close()
+}
+
+fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
+    for (msg in channel) {
+        delay(4000)
+        println("Processor #$id processeed $msg")
+    }
+}
+@ExperimentalCoroutinesApi
+suspend fun main(args: Array<String>) = withContext(Dispatchers.Default) {
+    val receiver = produceItems()
+
+    repeat(3) { launchProcessor(it, receiver)}
+}
