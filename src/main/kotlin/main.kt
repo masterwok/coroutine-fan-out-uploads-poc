be44c4e88028d0d00e29bafd2030c127e0d72da8
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2021-02-10 22:40:48 -0500

    Tweaked

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -17,12 +17,13 @@ private fun CoroutineScope.produceItems() = produce {
 fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
     for (msg in channel) {
         delay(4000)
-        println("Processor #$id processeed $msg")
+        println("Processor #$id processed $msg")
     }
 }
+
 @ExperimentalCoroutinesApi
 suspend fun main(args: Array<String>) = withContext(Dispatchers.Default) {
     val receiver = produceItems()
 
-    repeat(3) { launchProcessor(it, receiver)}
+    repeat(2) { launchProcessor(it, receiver)}
 }
